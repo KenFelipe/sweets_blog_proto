502fd390ffb34e32e60824c72bb2db807c85c2b2
Felipe Kenzo <Kenzo12152@gmail.com>
2021-06-01 20:11:01 -0300

    rm atoms/Text examples

--- src/components/atoms/Text/Text.styled.ts
+++
@@ -1,8 +0,0 @@
-import styled, { css } from 'styled-components'
-
-export const Container = styled.p`
-  ${({ theme }) => css`
-    font-size: ${theme.size.secondary};
-    color: ${theme.color.secondary};
-  `}
-`
--- src/components/atoms/Text/Text.tsx
+++
@@ -1,9 +0,0 @@
-import * as Styled from './Text.styled'
-
-export type TextProps = {
-  children: React.ReactNode
-}
-
-export const Text = ({ children }: TextProps) => {
-  return <Styled.Container>{children}</Styled.Container>
-}
