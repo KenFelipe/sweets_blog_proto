f9564f4e5a79b5d39b2a6203f32e2433ba97fb85
Felipe Kenzo <Kenzo12152@gmail.com>
2021-06-03 19:51:30 -0300

    refact mobileFirstParams

--- src/utils/storybook/mobileFirstViewport.ts
+++ src/utils/storybook/mobileFirstViewport.ts
@@ -2,45 +2,48 @@ import _ from 'lodash'
 
 import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport'
 
-const vpSetDefault = { ...MINIMAL_VIEWPORTS }
+export const mobileFirstViewportParams = (() => {
+  const vpSetDefault = { ...MINIMAL_VIEWPORTS }
 
-const customMobileLabel = {
-  mobile1: 'small',
-  mobile2: 'large',
-  tablet: 'tablet',
-}
+  const customMobileKey = {
+    // default: 'custom key'
+    mobile1: 'small',
+    mobile2: 'large',
+    tablet: 'tablet',
+  }
 
-const vpCustomSetToMerged = {
-  styles: {
-    border: '0',
-  },
-}
+  const vpCustomSetToMerged = {
+    styles: {
+      border: '0',
+    },
+  }
 
-const vpSetMerged = _.mapValues(vpSetDefault, obj =>
-  _.merge(obj, vpCustomSetToMerged),
-)
+  const vpSetMerged = _.mapValues(vpSetDefault, obj =>
+    _.merge(obj, vpCustomSetToMerged),
+  )
 
-const customVpSet = Object.keys(vpSetMerged).reduce(
-  (accumulator, key) => ({
-    ...accumulator,
-    [key]: {
-      layout: 'fullscreen',
-      viewport: {
-        defaultViewport: key,
-        viewports: {
-          ...vpSetMerged,
+  const customVpSet = Object.keys(vpSetMerged).reduce(
+    (accumulator, key) => ({
+      ...accumulator,
+      [key]: {
+        layout: 'fullscreen',
+        viewport: {
+          defaultViewport: key,
+          viewports: {
+            ...vpSetMerged,
+          },
         },
       },
-    },
-  }),
-  {},
-)
+    }),
+    {},
+  )
 
-const customVpSetWithCustomLabel = _.mapKeys(
-  customVpSet,
-  (_, key) => customMobileLabel[key],
-)
+  const customVpSetWithCustomKey = _.mapKeys(
+    customVpSet,
+    (_, key) => customMobileKey[key],
+  )
 
-export const mobileFirstViewportParams = {
-  ...customVpSetWithCustomLabel,
-}
+  return {
+    ...customVpSetWithCustomKey,
+  }
+})()
