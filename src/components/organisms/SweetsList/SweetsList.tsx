0762c3cfba36d5eba661899c48c28ee553934f8c
Felipe Kenzo <Kenzo12152@gmail.com>
2021-06-08 15:29:29 -0300

    Chg SweetsList to fetch from strapi gql api

--- src/components/organisms/SweetsList/SweetsList.tsx
+++ src/components/organisms/SweetsList/SweetsList.tsx
@@ -1,36 +1,45 @@
 import * as Styled from './SweetsList.styled'
+import { useEffect, useState } from 'react'
+
+import { client } from '@/api/connectServer'
+import { fetchSweets } from '@/api/fetchSweets'
 
 import {
   SweetsCard,
-  SweetsCardProps,
+  // SweetsCardProps,
 } from '@/molecules/SweetsCard/SweetsCard'
 
-export type SweetsData = Pick<
-  SweetsCardProps,
-  'name' | 'price' | 'imageUrl'
-> & { href: string }
+// export type SweetsData = Pick<
+//   SweetsCardProps,
+//   'name' | 'price' | 'imageUrl'
+// > & { href: string }
 
-export type SweetsListProps = {
-  sweetsListData: SweetsData[]
-}
+// export type SweetsListProps = {
+//   sweetsListData: SweetsData[]
+// }
 
-export const SweetsList = ({
-  sweetsListData = [],
-}: SweetsListProps) => {
+export const SweetsList = () => {
+  const [sweets, setSweets] = useState([])
+
+  useEffect(() => {
+    fetchSweets(client, 6, 6).then(({ data }) => {
+      setSweets(data.sweets)
+      console.log(JSON.stringify(data.sweets, null, 2))
+    })
+  }, [])
+
   return (
     <Styled.Wrapper>
-      {sweetsListData.map(
-        ({ name, price, imageUrl, href }, i) => (
-          <SweetsCard
-            key={i}
-            name={name}
-            price={price}
-            imageUrl={imageUrl}
-            isLink={true}
-            href={href}
-          />
-        ),
-      )}
+      {sweets.map(({ name, price, image }, i) => (
+        <SweetsCard
+          key={i}
+          name={name}
+          price={price}
+          imageUrl={image.url}
+          href={image.url}
+          isLink={true}
+        />
+      ))}
     </Styled.Wrapper>
   )
 }
